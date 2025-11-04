279707c2851b39d240c27df34c9e04dd7aaa3a9a
Enrique0321 <Enriqueluli193@gmail.com>
2025-11-04 17:49:17 -0600

    feat(roles): add action buttons (edit, delete, create) to roles table and fix breadcrumb links
    - Added edit and delete buttons in RoleTable and actions view
    
    - Created slot for "Nuevo" button in index view
    
    - Fixed breadcrumb links to use href instead of route

--- resources/views/admin/roles/index.blade.php
+++ resources/views/admin/roles/index.blade.php
@@ -5,6 +5,14 @@
     ],
     ['name' => 'Roles'],
 ]">
+
+<div class="flex justify-end mb-4">
+    <x-wire-button href="{{ route('admin.roles.create') }}" blue>
+        <i class="fa-solid fa-plus mr-2"></i> Nuevo rol
+    </x-wire-button>
+</div>
+
+
  @livewire('admin.datatables.role-table')
 </x-admin-layout>
 
